ce04c2f35a54cb16495d1260509a50c51ae75628
Jinsuo <cil04110@gmail.com>
2022-05-08 19:22:00 +0900

    Leetcode Daily Problem 22/05/08

---
+++ daily_problem/flatten_nested_list.cpp
@@ -0,0 +1,36 @@
+// 2022.05.08
+// https://leetcode.com/problems/flatten-nested-list-iterator/
+#include <bits/stdc++.h>
+using namespace std;
+class NestedIterator {
+public:
+  NestedIterator(vector<NestedInteger> &nestedList) {
+    begins.push(nestedList.begin());
+    ends.push(nestedList.end());
+  }
+
+  int next() {
+    hasNext();
+    return (begins.top()++)->getInteger();
+  }
+
+  bool hasNext() {
+    while (begins.size()) {
+      if (begins.top() == ends.top()) {
+        begins.pop();
+        ends.pop();
+      } else {
+        auto x = begins.top();
+        if (x->isInteger())
+          return true;
+        begins.top()++;
+        begins.push(x->getList().begin());
+        ends.push(x->getList().end());
+      }
+    }
+    return false;
+  }
+
+private:
+  stack<vector<NestedInteger>::iterator> begins, ends;
+};
