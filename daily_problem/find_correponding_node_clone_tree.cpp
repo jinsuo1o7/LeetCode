919e65a222755255050137a9e33adc3e89cb4d84
Jinsuo <cil04110@gmail.com>
2022-05-17 22:53:18 +0900

    Leetcode Daily Problem 22/05/17

---
+++ daily_problem/find_correponding_node_clone_tree.cpp
@@ -0,0 +1,20 @@
+// 2022.05.17
+// https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+  TreeNode *getTargetCopy(TreeNode *orig, TreeNode *clone, TreeNode *targ) {
+    return orig == nullptr ? nullptr :
+    orig == targ ? clone : 
+      getTargetCopy(orig->left, clone->left, targ) ?: getTargetCopy(orig->right, clone->right, targ);
+  }
+};
