ecc137774e080b705ddc2521c99b3fc696266b2e
Jinsuo <cil04110@gmail.com>
2022-05-18 17:28:53 +0900

    Leetcode Day 7 Graph

---
+++ graph__lv1/keys_rooms.cpp
@@ -0,0 +1,24 @@
+// Day 7
+// https://leetcode.com/problems/keys-and-rooms/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  bool canVisitAllRooms(vector<vector<int>> &rooms) {
+    stack<int> dfs;
+    dfs.push(0);
+    unordered_set<int> seen = {0};
+    while (!dfs.empty()) {
+      int i = dfs.top();
+      dfs.pop();
+      for (int j : rooms[i])
+        if (seen.count(j) == 0) {
+          dfs.push(j);
+          seen.insert(j);
+          if (rooms.size() == seen.size())
+            return true;
+        }
+    }
+    return rooms.size() == seen.size();
+  }
+};
