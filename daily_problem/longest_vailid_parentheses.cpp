476c4cec888d6fc664a78ae63f41cb10f3df4c63
Jinsuo <cil04110@gmail.com>
2022-05-24 17:19:13 +0900

    Leetcode Daily Problem 22/05/24

---
+++ daily_problem/longest_vailid_parentheses.cpp
@@ -0,0 +1,21 @@
+// 2022.05.24
+// https://leetcode.com/problems/longest-valid-parentheses/
+#include <bits/stdc++.h>
+using namespace std;
+int longestValidParentheses(string s) {
+  int MAX = 0; // denotes length of maximum valid parentheses
+  stack<int> stk;
+  stk.push(-1); // bottom of stack will always hold index preceding to potential
+                // start of valid parentheses
+  for (int i = 0; i < size(s); i++)
+    if (s[i] == '(')
+      stk.push(i);
+    else {
+      stk.pop();
+      if (stk.empty())
+        stk.push(i);
+      else
+        MAX = max(MAX, i - stk.top());
+    }
+  return MAX;
+}
