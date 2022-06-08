08745c2e7468a46f4bee9d37b7846655a1db0fb2
Jinsuo <cil04110@gmail.com>
2022-06-08 18:42:07 +0900

    Leetcode Daily Problem 22/06/08

---
+++ daily_problem/2022.06/remove_palindromic_subsequences.cpp
@@ -0,0 +1,19 @@
+// 2022.06.08
+// https://leetcode.com/problems/remove-palindromic-subsequences/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  int removePalindromeSub(string s) {
+    int i = 0, j = s.size() - 1;
+    while (i < j) {
+      // If string is not palindrome return 2
+      if (s[i] != s[j])
+        return 2;
+      i++;
+      j--;
+    }
+
+    return 1;
+  }
+};
