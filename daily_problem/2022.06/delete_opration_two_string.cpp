753461dc0c3e654e97048f9ba5d3d76c33e5d140
Jinsuo <cil04110@gmail.com>
2022-06-14 20:28:43 +0900

    Leetcode Daily Prolem 22/06/14

---
+++ daily_problem/2022.06/delete_opration_two_string.cpp
@@ -0,0 +1,24 @@
+// 2022.06.14
+// https://leetcode.com/problems/delete-operation-for-two-strings/
+#include <bits/stdc++.h>
+using namespace std;https://leetcode.com/problems/delete-operation-for-two-strings/
+class Solution {
+public:
+  vector<vector<int>> dp;
+  int minDistance(string word1, string word2) {
+    dp.resize(word1.size(), vector<int>(word2.size(), 1000));
+    return solve(word1, word2, 0, 0);
+  }
+  int solve(string &word1, string &word2, int i, int j) {
+    if (i == word1.size() && j == word2.size())
+      return 0;
+    if (i == word1.size() || j == word2.size())
+      return max(word1.size() - i, word2.size() - j);
+    if (dp[i][j] != 1000)
+      return dp[i][j];
+    if (word1[i] == word2[j])
+      return dp[i][j] = solve(word1, word2, i + 1, j + 1);
+    return dp[i][j] = 1 + min(solve(word1, word2, i + 1, j),
+                              solve(word1, word2, i, j + 1));
+  }
+};
