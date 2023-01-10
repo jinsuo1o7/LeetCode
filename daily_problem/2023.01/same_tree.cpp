e180b5d293497900277773031f276d4d817dbcd5
Jinsuo <cil04110@gmail.com>
2023-01-10 21:19:11 +0900

    same tree

---
+++ daily_problem/2023.01/same_tree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+	int val;
+	TreeNode* left, * right;
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
